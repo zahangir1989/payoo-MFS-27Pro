3f5057a3a4272465cf70fa89db0d2eba9282472a
zahangir <alamzahangir218@gmail.com>
2025-04-20 16:16:24 +0600

    add money section is running now

--- js/add-money.js
+++ js/add-money.js
@@ -1,31 +1,31 @@
 
-// document.getElementById('btn-add-money').addEventListener('click', function(event){
-//   event.preventDefault();
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+  event.preventDefault();
  
-//   const addMoneyInput = document.getElementById('input-add-money').value;
-//   const addMoneyPin = document.getElementById('input-add-pin').value;
-//   console.log(addMoneyInput, addMoneyPin);
+  const addMoneyInput = document.getElementById('input-add-money').value;
+  const addMoneyPin = document.getElementById('input-add-pin').value;
+  console.log(addMoneyInput, addMoneyPin);
 
-//   // pin verify
+  // pin verify
 
-//   if(addMoneyPin === '1234'){
-//     console.log('money is add your account')
+  if(addMoneyPin === '1234'){
+    console.log('money is add your account')
 
-//     const balance = document.getElementById('total-balance').innerText;
-//     console.log(balance)
+    const balance = document.getElementById('total-balance').innerText;
+    console.log(balance)
 
-//     const addMoneyParseFloat = parseFloat(addMoneyInput);
-//     const addBalanceParseFloat =parseFloat(balance);
-//     const newTotalMoney = addMoneyParseFloat + addBalanceParseFloat;
-//     console.log(newTotalMoney)
+    const addMoneyParseFloat = parseFloat(addMoneyInput);
+    const addBalanceParseFloat =parseFloat(balance);
+    const newTotalMoney = addMoneyParseFloat + addBalanceParseFloat;
+    console.log(newTotalMoney)
 
-//     // update total
-//     document.getElementById('total-balance').innerText=newTotalMoney;
-//     }
-//   else{
-//     alert('failed to your add money! Please try to again');
-//   }
-// })
+    // update total
+    document.getElementById('total-balance').innerText=newTotalMoney;
+    }
+  else{
+    alert('failed to your add money! Please try to again');
+  }
+})
 
-// // //  6 account update
-// // document.getElementById('balance').innerText=newBalance;
+// //  6 account update
+document.getElementById('balance').innerText=newBalance;
