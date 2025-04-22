e7a43812037ab51656c908f71b79dc52a2012744
zahangir <alamzahangir218@gmail.com>
2025-04-22 21:30:20 +0600

    cash out js section is added

--- js/cashOut.js
+++ js/cashOut.js
@@ -1,56 +1,56 @@
 
+document.getElementById('btn-cash-out').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log("cashOtu button is clicked");
+
+    const cashOutMoney = document.getElementById('cash-out-money').value;
+    const tParseFloat = parseFloat(cashOutMoney);
+    const cashOutPin = document.getElementById('cash-out-pin').value;
+    console.log(cashOutMoney, cashOutPin)
+
+    if(cashOutPin === '1234'){
+        console.log('cash out is reducing')
+
+        const balanceTotal = document.getElementById('total-balance').innerText;
+        console.log(balanceTotal)
+        const bParseFloat = parseFloat(balanceTotal)
+        const newBalance =  bParseFloat  -  tParseFloat;
+        console.log(newBalance)
+
+        document.getElementById('total-balance').innerText = newBalance;
+    }
+    else{
+        alert('failed to cash out');
+    }
+
+})
+
 // document.getElementById('btn-cash-out').addEventListener('click', function(event){
 //     event.preventDefault();
-//     console.log("cashOtu button is clicked");
+//     console.log('cashout is clicked')
 
-//     const cashOutMoney = document.getElementById('cash-out-money').value;
-//     const tParseFloat = parseFloat(cashOutMoney);
+//     const cashOut = document.getElementById('cash-out-money').value;
+//     const cashOutFloat = parseFloat(cashOut);
 //     const cashOutPin = document.getElementById('cash-out-pin').value;
-//     console.log(cashOutMoney, cashOutPin)
+//     console.log(cashOut, cashOutPin)
 
 //     if(cashOutPin === '1234'){
-//         console.log('cash out is reducing')
+//         console.log('cash out is reducing');
 
-//         const balanceTotal = document.getElementById('total-balance').innerText;
-//         console.log(balanceTotal)
-//         const bParseFloat = parseFloat(balanceTotal)
-//         const newBalance =  bParseFloat  -  tParseFloat;
-//         console.log(newBalance)
+//         const balance = document.getElementById('total-balance').innerText;
+//         console.log(balance)
+//         const balanceFloat = parseFloat(balance)
+       
 
-//         document.getElementById('total-balance').innerText = newBalance;
+       
+//         const newBalance = balanceFloat - cashOutFloat;
+        
+
+//         document.getElementById('total-balance').innerText=newBalance;
 //     }
 //     else{
 //         alert('failed to cash out');
 //     }
 
-// })
-
-// // document.getElementById('btn-cash-out').addEventListener('click', function(event){
-// //     event.preventDefault();
-// //     console.log('cashout is clicked')
-
-// //     const cashOut = document.getElementById('cash-out-money').value;
-// //     const cashOutFloat = parseFloat(cashOut);
-// //     const cashOutPin = document.getElementById('cash-out-pin').value;
-// //     console.log(cashOut, cashOutPin)
-
-// //     if(cashOutPin === '1234'){
-// //         console.log('cash out is reducing');
-
-// //         const balance = document.getElementById('total-balance').innerText;
-// //         console.log(balance)
-// //         const balanceFloat = parseFloat(balance)
-       
-
-       
-// //         const newBalance = balanceFloat - cashOutFloat;
-        
-
-// //         document.getElementById('total-balance').innerText=newBalance;
-// //     }
-// //     else{
-// //         alert('failed to cash out');
-// //     }
-
     
-// // })
+// })
